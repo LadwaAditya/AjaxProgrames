6e34065299ff8ba61c7de02ba4d73b210ccf3db4
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-02-29 23:02:29 +0530

    Created dataresponderpost.php and modified ajax4.html

--- dataresponder.php
+++ dataresponder.php
@@ -1,5 +1,5 @@
 <?php
-if($_GET['data'] == 1 ){
+if($_GET['data'] == 1){
   echo "You have sent the server a value of 1";
 }else if($_GET['data'] == 2){
   echo "You have sent the server a value of 2";
